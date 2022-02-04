c422439f16112e442b4cb9f894c3769b642aa04a
Azam Jawad Butt <azamjb123@gmail.com>
2022-02-04 17:31:01 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Azam Jawad on 2022-02-04.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
